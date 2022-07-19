39e3fb3e39dde76cf6075831286dbc50e4586f5a
James Macapagal <j.macapagal@gmail.com>
2022-07-18 20:14:37 -0500

    commit tree 72e0cdf2305feb67e1ad1b963829f6d8b5f03239

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    student_name = Student.find_by(name: params[:name])
+    students = if params[:name]
+                 Student.find_by(name: student_name)
+               else
+                 Student.all
+               end    
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
